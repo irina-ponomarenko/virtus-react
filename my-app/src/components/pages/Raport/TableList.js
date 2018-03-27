85d49095c3a0a5a4480022fe2f18262981aafce5
Admin <ponomarenko02041993@gmail.com>
2018-03-28 00:39:32 +0300

    virtus

--- my-app/src/components/pages/Raport/TableList.js
+++ my-app/src/components/pages/Raport/TableList.js
@@ -13,7 +13,8 @@ class TableList extends  React.Component{
                 text6: '$3.02',
                 text7: '$2.51',
                 text8: '$28.35',
-                text9: 'Active'
+                text9: 'Active',
+                status: 'StatusActive'
             },
             {
                 text1: 'Sed do eiusmod tempor',
@@ -24,7 +25,8 @@ class TableList extends  React.Component{
                 text6: '$8.45',
                 text7: '$6.13',
                 text8: '$45.22',
-                text9: 'Disable'
+                text9: 'Disable',
+                status: 'StatusDisabled'
             },
             {
                 text1: 'Consectetur adipisicing elit sed',
@@ -35,16 +37,15 @@ class TableList extends  React.Component{
                 text6: '$6.22',
                 text7: '$3.90',
                 text8: '$37.80',
-                text9: 'Active'
-            },
-
-
+                text9: 'Active',
+                status: 'StatusActive'
+            }
         ];
         return(
             <div className="TableList">
                 <table className="ProjectUserList">
                     <thead>
-                    <tr>
+                    <tr className="TableTitle">
                         <th>Campaing <i className="fa fa-chevron-down" aria-hidden="true"></i></th>
                         <th>Time <i className="fa fa-chevron-down" aria-hidden="true"></i></th>
                         <th>Views <i className="fa fa-chevron-down" aria-hidden="true"></i></th>
@@ -69,7 +70,7 @@ class TableList extends  React.Component{
                                     <td><h2>{item.text6}</h2></td>
                                     <td><h2>{item.text7}</h2></td>
                                     <td><h2>{item.text8}</h2></td>
-                                    <td><h2>{item.text9}</h2></td>
+                                    <td className="StatusClass"><i className={item.status +" fa fa-circle"} aria-hidden="true"></i> <h2>{item.text9}</h2></td>
                                 </tr>
                             )
                         })
