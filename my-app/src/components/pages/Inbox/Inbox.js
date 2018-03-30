9bd5232a9fd4e3239fa9c3801e3f692ebf912854
Admin <ponomarenko02041993@gmail.com>
2018-03-31 01:35:23 +0300

    virtus

--- my-app/src/components/pages/Inbox/Inbox.js
+++ my-app/src/components/pages/Inbox/Inbox.js
@@ -1,11 +1,35 @@
 import React  from 'react';
 import './Inbox.css';
+import Select from '../atoms/Select';
+import { Link } from 'react-router-dom';
+import InboxLis from './InboxList';
 
 class Inbox extends React.Component{
     render() {
+        const forPagesSelect = ["Date","Week","Month"];
         return(
-            <div className="Inbox">
-
+            <div className="WrapperProject">
+                <header className="PageHeader">
+                    <div className="TitleHeader">
+                        <Link to=" " className="marginLink">
+                            <i className="fa fa-inbox" aria-hidden="true"></i>
+                            <h2>Inbox (2)</h2>
+                        </Link>
+                        <Link  to=" " className="activeLink marginLink">
+                            <i className="fa fa-paper-plane" aria-hidden="true"></i>
+                            <h2>Sent</h2>
+                        </Link>
+                        <Link  to=" " className="activeLink marginLink">
+                            <i className="fa fa-trash" aria-hidden="true"></i>
+                            <h2>Trash</h2>
+                        </Link>
+                    </div>
+                    <div className="ButtonHeader">
+                        <h2>Filter messages:</h2>
+                        <Select data={forPagesSelect}/>
+                    </div>
+                </header>
+                <InboxLis/>
             </div>
         );
     }
---
+++ my-app/src/components/pages/Inbox/InboxList.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import Profile2 from '../../../assets/image/profileImg-2.png';
+import Profile3 from '../../../assets/image/profileImg-3.png';
+import Profile4 from '../../../assets/image/profileImg-4.png';
+import Profile5 from '../../../assets/image/profileImg-5.png';
+class InboxList extends React.Component{
+    render(){
+        const InboxList = [
+            {
+                icon: Profile2,
+                title: 'Michelle Stewart',
+                date: 'Today, 5:32 PM',
+                messageUser: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm.',
+                class: ''
+            },
+            {
+                icon: Profile3,
+                title: 'Jolene Slater',
+                date: '10 April',
+                messageUser: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.',
+                class: ''
+            },
+            {
+                icon: Profile5,
+                title: 'Lyall Roach',
+                date: '8 April',
+                messageUser: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+                class: 'OldMessage'
+            },
+            {
+                icon: Profile4,
+                title: 'Dominic Lynton',
+                date: '2 April',
+                messageUser: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.',
+                class: 'OldMessage'
+            },
+        ];
+        return(
+            <div className="InboxList">
+                {
+                    InboxList.map((item, index) =>{
+                        return(
+                            <div className="WrapperPreview" key={index}>
+                                <div className="PreviewMessage">
+                                    <div className="WrapperTitleMessage">
+                                        <div className="IconTitleMessage">
+                                            <img src={item.icon} alt="Profile"/>
+                                            <h2>{item.title}</h2>
+                                        </div>
+                                        <span className={item.class}>{item.date}</span>
+                                    </div>
+                                    <p>{item.messageUser}</p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+                <button type="button"><i className="fa fa-plus" aria-hidden="true"></i>New coversation</button>
+            </div>
+        );
+    }
+}
+export  default InboxList;
