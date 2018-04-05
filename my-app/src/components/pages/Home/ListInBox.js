248a647809c5daf8add33c6468323ce168934553
Admin <ponomarenko02041993@gmail.com>
2018-04-05 22:26:17 +0300

    new

--- my-app/src/components/pages/Home/ListInBox.js
+++ my-app/src/components/pages/Home/ListInBox.js
@@ -3,39 +3,61 @@ import IconImage1 from '../../../assets/image/profileImg-2.png';
 import IconImage2 from '../../../assets/image/profileImg-3.png';
 import IconImage3 from '../../../assets/image/profileImg-4.png';
 class ListInBox extends  React.Component{
-    render(){
-        const ListInBox = [
+    constructor() {
+        super();
+        // this.state = {
+        //     read: false
+        // };
+        this.ListInBox = [
             {
                 iconImage: IconImage1,
                 title: 'Michelle Stewart',
                 titleDate: 'Today, 5:32 PM',
                 text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm.',
-                classNew: ''
+                classNew: 'MessageCame'
             },
             {
                 iconImage: IconImage2,
                 title: 'Jolene Slater',
                 titleDate: 'Today, 5:32 PM',
                 text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm.',
-                classNew: ''
+                classNew: 'MessageCame'
             },
             {
                 iconImage: IconImage3,
                 title: 'Lyall Roach',
                 titleDate: 'Today, 5:32 PM',
                 text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm.',
-                classNew: 'OldMessage'
+                classNew: ''
             },
         ];
+
+    }
+    // messageClick = () => {
+    //     this.setState({
+    //         read: !this.state.read && MessageCame
+    //     });
+    // };
+
+
+    render(){
+
+        let MessageCameNumber = this.ListInBox.filter((item)=>{
+            if (item.classNew === 'MessageCame'){
+                return item;
+            }
+        });
+       let MessageCame = MessageCameNumber.length;
+
         return(
             <div className="Container">
                 <header className="HeaderContainer">
-                    <h2>Inbox (<span>2</span>)</h2>
+                    <h2>Inbox (<span>{MessageCame}</span>)</h2>
                 </header>
                 <div className="ScrollContainer">
                     {
-                        ListInBox.map((item, index) =>
-                            <div className="WrapperInbox" key={index}>
+                        this.ListInBox.map((item, index) =>
+                            <div className="WrapperInbox" key={index} onClick={this.messageClick}>
                                 <div className="IconUser">
                                     <img src={item.iconImage} alt="Icon user"/>
                                 </div>
